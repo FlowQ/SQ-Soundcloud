ab3c68ed97d0cd800ead737aefbfd9fb8c28432a
Florian Quattrocchi <florianquattrocchi@gmail.com>
2013-12-02 16:32:41 +0100

    Test pre commit

--- Scripts/getBlogs.php
+++ Scripts/getBlogs.php
@@ -6,51 +6,53 @@
     require_once ('../Config/config.php'); //prod
 	}
 
-    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-    $bdd = new PDO(DSN, DB_USERNAME, DB_PASSWORD, $pdo_options);
-    
-
-    $insSon = $bdd->prepare("INSERT INTO Blog (SCid, Blog) VALUES (:scid, :blog)");
-    $isDB = $bdd->prepare("SELECT count(TSsid) FROM Blog WHERE SCid = :scid");
-    $lastIdQuery = $bdd->prepare("SELECT SCid FROM Blog ORDER BY TSsid DESC LIMIT 1");
-    $lastIdQuery->execute();
-    
-
-
-	//initialisation
-	$srcUrl='http://www.musicmakesmyday.fr';
+	$blogURL='http://www.musicmakesmyday.fr';
 	$blogName = "MMMD";
-	$lastId = $lastIdQuery->fetch(PDO::FETCH_COLUMN, 0);
-	if(!$lastId)
-		$lastId=0;
+	parseSite($blogURL, $blogName);
 
+	$blogURL='http://www.chineurdesons.com';
+	$blogName = "CDSon";
+	parseSite($blogURL, $blogName);
 
-	$html = file_get_contents($srcUrl);
-	
-	$dom = new DOMDocument();
-	@$dom->loadHTML($html);
+	function parseSite($srcUrl, $blogName) {
+	    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+	    $bdd = new PDO(DSN, DB_USERNAME, DB_PASSWORD, $pdo_options);
+	    
 
-	// grab all the on the page
-	$xpath = new DOMXPath($dom);
+	    $insSon = $bdd->prepare("INSERT INTO Blog (SCid, Blog) VALUES (:scid, :blog)");
+	    $isDB = $bdd->prepare("SELECT count(TSsid) FROM Blog WHERE SCid = :scid AND Blog = :blog");
+	    $page = 1;
+	    $i = 1;
+	    $stop = false; //&& (!$stop)
+		while(($html = file_get_contents($srcUrl."/page/".$page))) {
+			echo $blogName;
+			$dom = new DOMDocument();
+			@$dom->loadHTML($html);
 
-	//finding the a tag
-	$hrefs = $xpath->evaluate("/html/body//iframe");
+			// grab all the on the page
+			$xpath = new DOMXPath($dom);
 
-	//Loop to display all the links
-	for ($i = 0; $i < $hrefs->length; $i++) {
-	       $href = $hrefs->item($i);
-	       $url = $href->getAttribute('src');
-	       //Filter the null links
-	       if($url!='#')
-	       {
-	       		$query = (parse_url($url));
-	       		$arg = explode("%2F", $query['query']);
-	       		$id = explode("&", $arg[4]);
-	       		$isDB->execute(array('scid' => $id[0]));
-	       		$count = $isDB->fetch(PDO::FETCH_COLUMN, 0);
-	       		if(!$count) {
-		       		$insSon->execute(array('scid' => $id[0], 'blog' => $blogName));
-		       	}
-	       }
+			//finding the a tag
+			$hrefs = $xpath->evaluate("/html/body//iframe");
+
+			//Loop to display all the links
+			for ($i = 0; $i < $hrefs->length; $i++) {
+		       $href = $hrefs->item($i);
+		       $url = $href->getAttribute('src');
+		       //Filter the null links
+		       if($url!='#' && strpos($url, 'soundcloud'))
+		       {
+		       		$query = (parse_url($url));
+		       		$arg = explode("%2F", $query['query']);
+		       		$id = explode("&", $arg[4]);
+		       		$isDB->execute(array('scid' => $id[0], 'blog' => $blogName));
+		       		$count = $isDB->fetch(PDO::FETCH_COLUMN, 0);
+		       		if(!$count) {
+			       		$insSon->execute(array('scid' => $id[0], 'blog' => $blogName));
+			       	} else { $stop = true;}
+		       }
+			}
+			$page++;
+		}
 	}
 ?>
